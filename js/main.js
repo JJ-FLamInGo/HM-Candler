52106121db4d3b9664f96caf16ad65a8c0a677dd
artem vartanyan <van.loner1@mail.ru>
2024-10-21 10:11:11 +0300

    added feature/main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuMobile = document.querySelector(".header__menu-list");
 
-menuBtn.addEventListener('click', ()=> {
-	menuMobile.classList.toggle('menu--open');
-})
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
